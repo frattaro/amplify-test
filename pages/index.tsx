e3dd46e5e8c76a3e5d0444fbecdbeae9b7bcaf91
Anton Frattaroli <anton@certedrive.com>
2022-12-22 09:14:41 -0500

    feat: basic supabase integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,33 @@
 import { Typography } from "@mui/material";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import Head from "next/head";
 
-import { AppPage } from "../components";
+import { Account, AppPage } from "../components";
 
 export default function Home() {
+  const session = useSession();
+  const supabase = useSupabaseClient();
+
   return (
     <>
       <Head>
         <title>Team Docs</title>
       </Head>
-
       <AppPage>
         <Typography variant="h1">Team Docs</Typography>
+
+        <div className="container" style={{ padding: "50px 0 100px 0" }}>
+          {!session ? (
+            <Auth
+              supabaseClient={supabase}
+              appearance={{ theme: ThemeSupa }}
+              theme="dark"
+            />
+          ) : (
+            <Account session={session} />
+          )}
+        </div>
 
         <Typography variant="h2">Meet the Team</Typography>
 
